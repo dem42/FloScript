0c4f7ce0c3a9411de450e49e85932ddd287d8bf1
dem42 <none@none>
2015-02-23 23:13:09 +0000

    better background

--- app/src/main/java/com/premature/floscript/util/FloColors.java
+++ app/src/main/java/com/premature/floscript/util/FloColors.java
@@ -7,7 +7,7 @@ import android.graphics.Color;
  */
 public class FloColors {
     public static final int backgroundColor = Color.parseColor("#e9e9e9");
-    public static final int elemColor = Color.parseColor("#0746B6");//Color.parseColor("#3399CC");
+    public static final int elemColor = Color.parseColor("#0077cc");//Color.parseColor("#0746B6");//Color.parseColor("#3399CC");
     public static final int highlightColor = Color.parseColor("#330000");
     public static final int textColor = Color.WHITE;
 }
